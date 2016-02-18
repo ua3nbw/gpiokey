5d599acbe8e29fd09359fd64461a51097fb64ba3
Valery <ua3nbw@gmail.com>
2016-02-18 14:09:38 +0300

    Update wpi.c

--- wpi.c
+++ wpi.c
@@ -4,7 +4,8 @@
 #include <stdlib.h>
 #include <wiringPi.h>
 
-// Button connected to PA8, ie. Physical OPi-pin 31                    
+// WPi 7 to PA7, ie. Physical OPi-pin 29||WPi 8 to PA8, ie. Physical OPi-pin 31||WPi 9 to PG08 OPi-pin 32||
+// WPi 10 to PA09 OPi-pin 33|| WPi 12 to PPA10 OPi-pin 35  || WPi 15 to PG06 OPi-pin 38                     
 #define BUTTON_PIN 8
 
 // the event counter
